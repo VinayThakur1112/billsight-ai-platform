f51e3b5ceedf908390e2584d4ff365725e73b2a6
Vinay Thakur <vinayrsinghthakur@gmail.com>
2026-01-13 20:56:54 +0530

    data extraction logic added

--- services/transform/main.py
+++ services/transform/main.py
@@ -15,10 +15,17 @@ PROJECT_ID = os.getenv("PROJECT_ID")
 DATASET = os.getenv("BQ_DATASET")
 SOURCE_TABLE = os.getenv("BQ_RAW_OCR_TABLE")
 
+
+def extract(pattern, text, group=1):
+    match = re.search(pattern, text, re.IGNORECASE | re.MULTILINE)
+    return match.group(group).strip() if match else None
+
+
 def fetch_raw_ocr_rows(limit=1):
     query = f"""
     SELECT
       correlation_id,
+      file_name,
       text
     FROM `{PROJECT_ID}.{DATASET}.{SOURCE_TABLE}`
     WHERE text IS NOT NULL
@@ -27,12 +34,70 @@ def fetch_raw_ocr_rows(limit=1):
     """
     return client.query(query).result()
 
+def normalize_amount(value: str) -> float:
+    return float(
+        value.replace(" ", "").replace(",", ".")
+    )
+
+
 def run_transformer():
     rows = list(fetch_raw_ocr_rows())
     logger.info(f"Fetched {len(rows)} rows")
 
     for row in rows:
         print(row)
+
+        data = {}
+
+        data["invoice_number"] = extract(
+            r"Invoice\s*no[:\s]+(\d+)", row.text
+        )
+
+        data["date_of_issue"] = extract(
+            r"Date of issue[:\s]*\n?([0-9/.-]+)", row.text
+        )
+
+        seller_block = extract(
+            r"Seller:\s*(.*?)\n\s*Client:",
+            row.text,
+            group=1
+        )
+        logger.info(f'seller_block: {seller_block}')
+
+        if seller_block:
+            lines = [
+                l.strip() for l in seller_block.splitlines() if l.strip()
+            ]
+            data["seller_name"] = lines[0]
+            data["seller_address"] = " ".join(lines[1:-1])
+            data["seller_tax_id"] = extract(
+                r"Tax Id[:\s]+([\d-]+)", seller_block
+            )
+        
+        client_block = extract(
+            r"Client:\s*(.*?)\n\s*ITEMS",
+            row.text,
+            group=1
+        )
+
+        if client_block:
+            lines = [
+                l.strip() for l in client_block.splitlines() if l.strip()
+            ]
+            data["client_name"] = lines[0]
+            data["client_address"] = " ".join(lines[1:-1])
+            data["client_tax_id"] = extract(
+                r"Tax Id[:\s]+([\d-]+)", client_block
+            )
+
+        totals = re.findall(r"\$\s*([\d\s.,]+)", row.text)
+
+        if len(totals) >= 3:
+            data["total_net"] = normalize_amount(totals[-3])
+            data["total_vat"] = normalize_amount(totals[-2])
+            data["total_gross"] = normalize_amount(totals[-1])
+
+        logger.info(f"Extracted Data: {data}")
 
 if __name__ == "__main__":
     run_transformer()
